8a197e32a5f4d82e1a7edd2bad9c21097d985f67
Vigneshvicky9787 <vicky5760rr@gmail.com>
2021-12-30 23:47:14 +0530

    promises and it's types

--- newjs.js
+++ newjs.js
@@ -31,5 +31,22 @@ let newfilterArr = ranks.filter((e,index) => {
 console.log(newfilterArr)
 
 
+// --------------------------------------------------
 
+let concert = false;
+
+const attendConcert = new Promise((resolve, reject) => {
+  setTimeout(() => {
+if(concert){
+  resolve ('bob attended the concert')
+}
+else{
+  reject ("bob didn't attend the concert")
+}
+  },2000)
+}
+)
+console.log(attendConcert)
+
+attendConcert.then((data) => console.log(data)) .catch((error) => console.log(error))
 
